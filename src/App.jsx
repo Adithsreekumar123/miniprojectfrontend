3b25b9753708a6783610e20873d032df2a8b7627
ZENZEPHY <adithkvxid@gmail.com>
2025-03-28 14:59:32 +0530

    Refactor App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Search from './component/search'
-import Navbar from './component/Navbar'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Search from "./component/search";
+import Navbar from "./component/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <><Search /><Navbar /></>
-    
-  )
+    <>
+      <Search />
+      <Navbar />
+    </>
+  );
 }
 
-export default App
+export default App;
